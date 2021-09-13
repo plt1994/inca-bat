9860710df18df410c1a4f14c2624b1f3dcb34835
plt1994 <patlopez@ug.uchile.cl>
2021-09-13 01:57:48 -0300

    feat: added new options for settings

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -1,17 +1,56 @@
 <script>
     import { Link } from "svelte-navigator";
-    import { nOfRepetitions } from "./stores.js";
-    let n_of_repetitions;
-    function increment() {
-        nOfRepetitions.update((n) => n + 1);
+    import {
+        nOfRepetitions,
+        nOfCardsOnScreen,
+        cardsSeparation,
+        subjectName,
+        loggingStyle,
+    } from "./stores.js";
+    const logStyleStr = {
+        0: "Verbose (every selection)",
+        1: "Simple (only results)",
+    };
+    let localNOfRepetitions,
+        localCardsOnScreen,
+        localCardsSeparation,
+        localSubjectName,
+        localLoggingStyle;
+    const options = {
+        0: nOfRepetitions,
+        1: nOfCardsOnScreen,
+        2: cardsSeparation,
+        3: subjectName,
+        4: loggingStyle,
+    };
+    function increment(option) {
+        options[option].update((n) => {
+            return n + 1;
+        });
     }
-
-    function decrement() {
-        nOfRepetitions.update((n) => n - 1);
+    function decrement(option) {
+        options[option].update((n) => {
+            if (n < 2) {
+                return n;
+            }
+            return n - 1;
+        });
     }
 
     nOfRepetitions.subscribe((value) => {
-        n_of_repetitions = value;
+        localNOfRepetitions = value;
+    });
+    nOfCardsOnScreen.subscribe((value) => {
+        localCardsOnScreen = value;
+    });
+    cardsSeparation.subscribe((value) => {
+        localCardsSeparation = value;
+    });
+    subjectName.subscribe((value) => {
+        localSubjectName = value;
+    });
+    loggingStyle.subscribe((value) => {
+        localLoggingStyle = value;
     });
 </script>
 
@@ -19,9 +58,37 @@
     Number of repetitions
 
     <div>
-        <button on:click={increment}> + </button>
-        <button> {n_of_repetitions} </button>
-        <button on:click={decrement}> - </button>
+        <button on:click={() => increment(0)}> + </button>
+        <button> {localNOfRepetitions} </button>
+        <button on:click={() => decrement(0)}> - </button>
+    </div>
+
+    Cards on screen
+
+    <div>
+        <button on:click={() => increment(1)}> + </button>
+        <button> {localCardsOnScreen} </button>
+        <button on:click={() => decrement(1)}> - </button>
+    </div>
+
+    Cards separation
+
+    <div>
+        <button on:click={() => increment(2)}> + </button>
+        <button> {localCardsSeparation} </button>
+        <button on:click={() => decrement(2)}> - </button>
+    </div>
+
+    Subject name
+
+    <div>
+        <button> {localSubjectName} </button>
+    </div>
+
+    Logging Style
+
+    <div>
+        <button> {logStyleStr[localLoggingStyle]} </button>
     </div>
 </div>
 
