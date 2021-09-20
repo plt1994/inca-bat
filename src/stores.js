6fcee2590442061de23ee0a3a2191e712efe0843
plt1994 <patlopez@ug.uchile.cl>
2021-09-20 03:09:30 -0300

    feat: saved most important stores constants on client local storage

--- src/stores.js
+++ src/stores.js
@@ -1,25 +1,77 @@
 import { writable } from 'svelte/store';
 
+//check for client local storage store values
 const storedSubjectName = localStorage.subjectName
+const storedNOfRepetitions = localStorage.nOfRepetitions
+const storedLoggingStyle = localStorage.loggingStyle
+const storedCardsSeparation = localStorage.cardsSeparation
+const storedNOfCardsOnScreen = localStorage.nOfCardsOnScreen
+const storedTimeHoldToExit = localStorage.timeHoldToExit
+const storedShowTitle = localStorage.showTitle
+const storedShowCardText = localStorage.showCardText
+const storedSilenceVoice = localStorage.silenceVoice
+const storedSilenceFeedback = localStorage.silenceFeedback
+const storedCurrentFeedbackSound = localStorage.currentFeedbackSound
+const storedSaveLog = localStorage.saveLog
+const storedCardH = localStorage.cardH
+const storedCardW = localStorage.cardW
 
-export const count = writable(0);
-export const nOfRepetitions = writable(5)
+export const nOfRepetitions = writable(Number(storedNOfRepetitions) || 5)
 export const selectedTest = writable(0)
-export const loggingStyle = writable(0) //0 for verbose, 1 for simple
+export const loggingStyle = writable(Number(storedLoggingStyle) || 0) //0 for verbose, 1 for simple
 export const subjectName = writable(storedSubjectName || "Subject")
-export const cardsSeparation = writable(5) //1 to 10
-export const nOfCardsOnScreen = writable(2)
-export const timeHoldToExit = writable(2)
+export const cardsSeparation = writable(Number(storedCardsSeparation) || 5) //1 to 10
+export const nOfCardsOnScreen = writable(Number(storedNOfCardsOnScreen) || 2)
+export const timeHoldToExit = writable(Number(storedTimeHoldToExit) || 2)
 export const showTitle = writable(false)
-export const showCardText = writable(true)
-export const silenceVoice = writable(false)
-export const silenceFeedback = writable(false)
-export const currentFeedbackSound = writable(1)
+export const showCardText = writable(false)
+export const silenceVoice = writable(Boolean(storedSilenceVoice) || false)
+export const silenceFeedback = writable(Boolean(storedSilenceFeedback) || false)
+export const currentFeedbackSound = writable(Number(storedCurrentFeedbackSound) || 1)
 export const saveLog = writable(true)
-export const cardH = writable(180)
-export const cardW = writable(160)
+export const cardH = writable(Number(storedCardH) || 180)
+export const cardW = writable(Number(storedCardW) || 160)
 
 subjectName.subscribe((value) => {
-    console.log(localStorage)
     localStorage.subjectName = value
+})
+
+nOfRepetitions.subscribe((value) => {
+    localStorage.nOfRepetitions = value
+})
+
+loggingStyle.subscribe((value) => {
+    localStorage.loggingStyle = value
+})
+
+cardsSeparation.subscribe((value) => {
+    localStorage.cardsSeparation = value
+})
+
+nOfCardsOnScreen.subscribe((value) => {
+    localStorage.nOfCardsOnScreen = value
+})
+
+timeHoldToExit.subscribe((value) => {
+    localStorage.timeHoldToExit = value
+})
+
+silenceVoice.subscribe((value) => {
+    localStorage.silenceVoice = value
+})
+
+silenceFeedback.subscribe((value) => {
+    localStorage.silenceFeedback = value
+})
+
+currentFeedbackSound.subscribe((value) => {
+    localStorage.currentFeedbackSound = value
+})
+
+cardH.subscribe((value) => {
+    localStorage.cardH = value
+})
+
+cardW.subscribe((value) => {
+    localStorage.cardW = value
 })
