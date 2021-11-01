aa429348a811a93dde5ad20c27cf6d2b7022d7ab
plt1994 <patlopez@ug.uchile.cl>
2021-11-01 19:26:30 -0300

    feat: added simple footer component

---
+++ src/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Navbar, NavbarBrand, Col, Container, Row } from "sveltestrap";
+    let color = "light";
+</script>
+
+<footer class="fixed-bottom">
+    <Container>
+        <Row>
+            <Navbar {color}><slot /></Navbar>
+        </Row>
+    </Container>
+</footer>
+
+<style>
+</style>
