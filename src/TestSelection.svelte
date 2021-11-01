4efb89e7c970bdcabe7b1c0eca57638d4e0e88b4
plt1994 <patlopez@ug.uchile.cl>
2021-11-01 19:31:41 -0300

    feat: updated test selection footer

--- src/TestSelection.svelte
+++ src/TestSelection.svelte
@@ -2,32 +2,23 @@
     import TestDisplayer from "./TestDisplayer.svelte";
     import Link from "./Link.svelte";
     import { moveToPage } from "./navigator";
+    import { Navbar } from "sveltestrap";
 </script>
 
 <center><h1>Select a Test to start</h1></center>
 <center>
     <div class="color">
         <TestDisplayer />
-        <button class="buttonclass">
-            <Link to="card_selection">Start selected test</Link>
-        </button>
     </div>
 </center>
 
-<center><button on:click={() => moveToPage("menu")}>Go Back</button></center>
-
-<style>
-    /* .color {
-        background-color: rgb(195, 207, 207);
-        display: grid;
-        place-items: center;
-        margin-left: 10%;
-        margin-right: 10%;
-        margin-top: 5%;
-    } */
-
-    .buttonclass {
-        /* background-color: rgb(80, 75, 83); */
-        font-size: 7vw;
-    }
-</style>
+<footer class="fixed-bottom">
+    <Navbar>
+        <button class="col-sm-6" on:click={() => moveToPage("menu")}
+            >Go Back</button
+        >
+        <button class="col-sm-6">
+            <Link to="card_selection">Start selected test</Link>
+        </button>
+    </Navbar>
+</footer>
