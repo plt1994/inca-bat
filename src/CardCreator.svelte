1a1c1e614edb1e75e07ac0f2c9c402c41f05e299
plt1994 <patlopez@ug.uchile.cl>
2021-11-01 19:25:53 -0300

    feat: added emoji selector

--- src/CardCreator.svelte
+++ src/CardCreator.svelte
@@ -1,11 +1,28 @@
 <script>
     import Link from "./Link.svelte";
     import { localCards } from "./stores";
+    import EmojiSelector from "svelte-emoji-selector";
     export let createNew = false;
     export let width = 100;
     export let height = 100;
     let imgSrc;
+    let isLoaded = false;
+    let imgCode;
     let cardName;
+    let textContent = "";
+
+    function onEmoji(event) {
+        textContent = event.detail;
+        imgCode = emojiUnicode(textContent);
+        imgSrc = getImgSrc();
+        return imgCode;
+    }
+    function getImgSrc() {
+        if (imgCode) {
+            return `https://emojiapi.dev/api/v1/${imgCode}.svg`;
+        }
+        return null;
+    }
     function addNewCard() {
         let newId = 0;
         for (let i = 0; i < $localCards.length; i++) {
@@ -25,16 +42,35 @@
         };
         let newLocalCards = $localCards;
         newLocalCards.push(newCardData);
+        isLoaded = false;
         localCards.update(() => {
             return newLocalCards;
         });
+    }
+    function emojiUnicode(emoji) {
+        var comp;
+        if (emoji.length === 1) {
+            comp = emoji.charCodeAt(0);
+        }
+        comp =
+            (emoji.charCodeAt(0) - 0xd800) * 0x400 +
+            (emoji.charCodeAt(1) - 0xdc00) +
+            0x10000;
+        if (comp < 0) {
+            comp = emoji.charCodeAt(0);
+        }
+        return comp.toString("16");
     }
 </script>
 
 <center>
     {#if createNew}
         <div>Card Creator</div>
-
+        <div class="instructions">Choose an Emoji or put url</div>
+        <div class="align">
+            <p>Create from emoji:</p>
+            <EmojiSelector on:emoji={onEmoji} />
+        </div>
         <label
             >Image source (url):<input bind:value={imgSrc} type="text" /></label
         >
@@ -45,12 +81,23 @@
             /></label
         >
         <div class="preview-card-creator">
-            <img src={imgSrc} alt={cardName} />
+            <img
+                id="img-preview"
+                on:load={() => (isLoaded = true)}
+                on:error={() => (isLoaded = false)}
+                src={imgSrc}
+                alt={cardName}
+            />
         </div>
-        <button on:click={addNewCard} class="button-style">Save!</button>
-        <Link to="test_creator"
-            ><button class="button-style">Go back</button></Link
-        >
+        <div class="align">
+            {#if isLoaded}
+                <button on:click={addNewCard} class="button-style">Save!</button
+                >
+            {/if}
+            <Link to="test_creator"
+                ><button class="button-style">Go back</button></Link
+            >
+        </div>
     {:else}
         <Link to="new_card"
             ><button class="new-card-button">New card</button></Link
@@ -59,11 +106,18 @@
 </center>
 
 <style>
+    .instructions {
+        background-color: darkgrey;
+    }
+    .align {
+        display: inline-flex;
+    }
     .new-card-button {
         height: 100px;
         width: 100px;
         border-radius: 10px;
         margin: 0;
+        background-color: darkseagreen;
     }
     .button-style {
         height: 50px;
