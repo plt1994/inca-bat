e060cc5a8e0228a7dd4a09933c1b647aca6e8cb8
plt1994 <patlopez@ug.uchile.cl>
2021-11-21 04:04:53 -0300

    feat: added all dice cards

--- src/components/Card/DiceCard.svelte
+++ src/components/Card/DiceCard.svelte
@@ -24,45 +24,6 @@
     }
 </script>
 
-<!-- {#if mode == "dot"}
-    <svg {width} {height}>
-        <defs>
-            <linearGradient id="g1" x1="0%" y1="0%" x2="100%" y2="100%">
-                <stop stop-color={base_color_face} offset="0%" />
-                <stop stop-color={base_color_face} offset="100%" />
-            </linearGradient>
-            <radialGradient id="g2" cx="50%" cy="50%" r="50%" fx="40%" fy="60%">
-                <stop stop-color="white" offset="0%" />
-                <stop stop-color={base_color_dot} offset="00%" />
-                <stop stop-color={base_color_dot} offset="100%" />
-                <stop stop-color="transparent" offset="100%" />
-            </radialGradient>
-        </defs>
-        <rect x="0" y="0" rx={width / 8} ry={height / 8} {width} {height} />
-        <g transform="translate(5,5)">
-            {#if [2, 3, 4, 5, 6, 7, 8, 9].indexOf(id) != -1}
-                <circle cx={width / 5} cy={height / 5} r={radius} />
-            {/if}
-            {#if [1, 3, 5, 7, 8, 9].indexOf(id) != -1}
-                <circle cx={id*width / 3} cy={id*height / 3} r={radius} />
-            {/if}
-        </g>
-    </svg>
-    <style>
-        rect {
-            stroke: black;
-            stroke-width: 5;
-            fill: url(#g1);
-        }
-        circle {
-            stroke: black;
-            stroke-width: 0;
-            fill: url(#g2);
-            opacity: 0.8;
-        }
-    </style>
-{/if} -->
-
 <svg {width} {height}>
     <rect
         x="5"
@@ -75,54 +36,54 @@
         fill-opacity="1"
     />
     {#if id == 9}
-        <circle cx="32" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="75" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="75" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="75" cy="118" r={radius} fill={base_color_dot} />
+        <Circle x="1" y="1" {radius} w={width} h={height} />
+        <Circle x="1" y="3" {radius} w={width} h={height} />
+        <Circle x="2" y="1" {radius} w={width} h={height} />
+        <Circle x="2" y="3" {radius} w={width} h={height} />
+        <Circle x="3" y="1" {radius} w={width} h={height} />
+        <Circle x="3" y="3" {radius} w={width} h={height} />
+        <Circle x="2" y="2" {radius} w={width} h={height} />
+        <Circle x="3" y="2" {radius} w={width} h={height} />
+        <Circle x="1" y="2" {radius} w={width} h={height} />
     {:else if id == 8}
-        <circle cx="32" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="75" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="75" cy="118" r={radius} fill={base_color_dot} />
+        <Circle x="1" y="1" {radius} w={width} h={height} />
+        <Circle x="1" y="3" {radius} w={width} h={height} />
+        <Circle x="1" y="2" {radius} w={width} h={height} />
+        <Circle x="3" y="2" {radius} w={width} h={height} />
+        <Circle x="3" y="1" {radius} w={width} h={height} />
+        <Circle x="3" y="3" {radius} w={width} h={height} />
+        <Circle x="2" y="2" {radius} w={width} h={height} />
+        <Circle x="2" y="3" {radius} w={width} h={height} />
     {:else if id == 7}
-        <circle cx="32" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="75" cy="75" r={radius} fill={base_color_dot} />
+        <Circle x="1" y="1" {radius} w={width} h={height} />
+        <Circle x="1" y="3" {radius} w={width} h={height} />
+        <Circle x="1" y="2" {radius} w={width} h={height} />
+        <Circle x="3" y="2" {radius} w={width} h={height} />
+        <Circle x="3" y="1" {radius} w={width} h={height} />
+        <Circle x="3" y="3" {radius} w={width} h={height} />
+        <Circle x="2" y="2" {radius} w={width} h={height} />
     {:else if id == 6}
-        <circle cx="32" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="118" r={radius} fill={base_color_dot} />
+        <Circle x="1" y="1" {radius} w={width} h={height} />
+        <Circle x="1" y="3" {radius} w={width} h={height} />
+        <Circle x="1" y="2" {radius} w={width} h={height} />
+        <Circle x="3" y="2" {radius} w={width} h={height} />
+        <Circle x="3" y="1" {radius} w={width} h={height} />
+        <Circle x="3" y="3" {radius} w={width} h={height} />
     {:else if id == 5}
-        <circle cx="32" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="75" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="118" r={radius} fill={base_color_dot} />
+        <Circle x="1" y="1" {radius} w={width} h={height} />
+        <Circle x="1" y="3" {radius} w={width} h={height} />
+        <Circle x="3" y="1" {radius} w={width} h={height} />
+        <Circle x="3" y="3" {radius} w={width} h={height} />
+        <Circle x="2" y="2" {radius} w={width} h={height} />
     {:else if id == 4}
-        <circle cx="32" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="32" cy="118" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="118" r={radius} fill={base_color_dot} />
+        <Circle x="1" y="1" {radius} w={width} h={height} />
+        <Circle x="1" y="3" {radius} w={width} h={height} />
+        <Circle x="3" y="1" {radius} w={width} h={height} />
+        <Circle x="3" y="3" {radius} w={width} h={height} />
     {:else if id == 3}
-        <circle cx="32" cy="32" r={radius} fill={base_color_dot} />
-        <circle cx="75" cy="75" r={radius} fill={base_color_dot} />
-        <circle cx="118" cy="118" r={radius} fill={base_color_dot} />
+        <Circle x="1" y="1" {radius} w={width} h={height} />
+        <Circle x="2" y="2" {radius} w={width} h={height} />
+        <Circle x="3" y="3" {radius} w={width} h={height} />
     {:else if id == 2}
         <Circle x="1" y="1" {radius} w={width} h={height} />
         <Circle x="3" y="3" {radius} w={width} h={height} />
--- src/controller/cards.js
+++ src/controller/cards.js
@@ -32,5 +32,68 @@ export const cards = [
             mode: "simple",
             n: 2
         },
+    },
+    {
+        id: 4,
+        cardName: "3 dots card",
+        cardType: "diceCard",
+        details: {
+            mode: "simple",
+            n: 3
+        },
+    },
+    {
+        id: 5,
+        cardName: "4 dots card",
+        cardType: "diceCard",
+        details: {
+            mode: "simple",
+            n: 4
+        },
+    },
+    {
+        id: 6,
+        cardName: "5 dots card",
+        cardType: "diceCard",
+        details: {
+            mode: "simple",
+            n: 5
+        },
+    },
+    {
+        id: 7,
+        cardName: "6 dots card",
+        cardType: "diceCard",
+        details: {
+            mode: "simple",
+            n: 6
+        },
+    },
+    {
+        id: 8,
+        cardName: "7 dots card",
+        cardType: "diceCard",
+        details: {
+            mode: "simple",
+            n: 7
+        },
+    },
+    {
+        id: 9,
+        cardName: "8 dots card",
+        cardType: "diceCard",
+        details: {
+            mode: "simple",
+            n: 8
+        },
+    },
+    {
+        id: 10,
+        cardName: "9 dots card",
+        cardType: "diceCard",
+        details: {
+            mode: "simple",
+            n: 9
+        },
     }
 ]
